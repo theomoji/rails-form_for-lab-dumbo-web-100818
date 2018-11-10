8eb8bfe1147c2df97b51355a0fddf6838a77dcdb
github username <github email address>
2018-11-10 18:25:17 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :students
+  # , except: [:destroy, :index, :new, :create]
+  resources :school_classes
+  # , except: [:destroy, :index, :new, :create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
---
+++ spec/controllers/student_controller_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+RSpec.describe StudentsController, type: :controller do
+
+
+end
